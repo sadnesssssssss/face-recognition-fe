f1186e4f1a51ecc2bac2fdd7dd6681c52dc66807
askdjfhasdkjfh <77674821+sadnesssssssss@users.noreply.github.com>
2024-05-18 13:30:27 +0300

    minor code fixes

--- src/components/MainPage/Faq.tsx
+++ src/components/MainPage/Faq.tsx
@@ -1,5 +1,5 @@
 import "./Faq.css";
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Faq = () => {
     const [isVisible1, setIsVisible1] = useState(false);
@@ -7,20 +7,10 @@ const Faq = () => {
     const [isVisible3, setIsVisible3] = useState(false);
     const [isVisible4, setIsVisible4] = useState(false);
 
-    const toggleVisibility = ({NumButton, Setter}: { NumButton: number, Setter: any }) => {
-        if (NumButton === 1){
-            setIsVisible1(!Setter)
-        }
-        if (NumButton === 2){
-            setIsVisible2(!Setter)
-        }
-        if (NumButton === 3){
-            setIsVisible3(!Setter)
-        }
-        if (NumButton === 4){
-            setIsVisible4(!Setter)
-        }
+    const toggleVisibility = ({Setter, Value}: {Setter: any, Value: any }) => {
+        Setter(!Value);
     };
+
     return <section className="sorting-sanctuary">
         <div className="faq1" data-scroll-to="faqContainer">
             <div className="analyze-arena">
@@ -31,51 +21,49 @@ const Faq = () => {
             <div className="merge-mosaic">
                 <div className="pattern-parade">
                     <button className="button-faq" onClick={() => toggleVisibility({
-                        NumButton: 1,
-                        Setter: isVisible1
+                        Setter: setIsVisible1, Value: isVisible1
                     })}>
                         <div className="calculate-cube1">
                             <img
-                                className="ouiarrow-up-icon"
+                                className={"ouiarrow-up-icon"}
                                 loading="lazy"
-                                alt="Flipped Image"
+                                alt=""
                                 src="/icons/arrowup.svg"
-                                style={{transform: isVisible1 ? 'rotate(180deg)' : 'rotate(0deg)'}}
+                                style={{transform: isVisible1 ? 'rotate(0deg)' : 'rotate(180deg)'}}
                             />
                             <div className="what-is-face">What is Face Recognition?</div>
                         </div>
                     </button>
-                    {isVisible1 && <p className="data-aggregator-p">
-                            <div className="data-aggregator">
-                                <div className="face-recognition-is">
-                                    Face Recognition is a biometric identification technology that
-                                    recognizes and identifies people's faces in images or videos.
-                                    Using computer vision and deep learning algorithms, face
-                                    recognition systems can analyze unique facial features such as
-                                    facial shape, eye shape, eye location, nose and mouth to create
-                                    a unique biometric template.
-                                </div>
+                    {isVisible1 && <div id="text1" className="data-aggregator-p">
+                        <div className="data-aggregator">
+                            <div className="face-recognition-is">
+                                Face Recognition is a biometric identification technology that
+                                recognizes and identifies people's faces in images or videos.
+                                Using computer vision and deep learning algorithms, face
+                                recognition systems can analyze unique facial features such as
+                                facial shape, eye shape, eye location, nose and mouth to create
+                                a unique biometric template.
                             </div>
-                    </p>}
+                        </div>
+                    </div>}
                 </div>
                 <div className="pattern-parade">
                     <button className="button-faq" onClick={() => toggleVisibility({
-                        NumButton: 2,
-                        Setter: isVisible2
+                        Setter: setIsVisible2, Value: isVisible2
                     })}>
                         <div className="calculate-cube1">
                             <img
                                 className="ouiarrow-up-icon"
                                 alt=""
                                 src="/icons/arrowup.svg"
-                                style={{transform: isVisible2 ? 'rotate(180deg)' : 'rotate(0deg)'}}
+                                style={{transform: isVisible2 ? 'rotate(0deg)' : 'rotate(180deg)'}}
                             />
                             <div className="what-is-face">
                                 How many devices can I use at the same time in IdentityX?
                             </div>
                         </div>
                     </button>
-                    {isVisible2 && <p className="data-aggregator-p">
+                    {isVisible2 && <div id="text2" className="data-aggregator-p">
                         <div className="data-aggregator">
                             <div className="with-a-premium-container">
                                 <p className="with-a-premium">
@@ -89,12 +77,11 @@ const Faq = () => {
                                 </p>
                             </div>
                         </div>
-                    </p>}
+                    </div>}
                 </div>
                 <div className="pattern-parade">
                     <button className="button-faq" onClick={() => toggleVisibility({
-                        NumButton: 3,
-                        Setter: isVisible3
+                        Setter: setIsVisible3, Value: isVisible3
                     })}>
                         <div className="event-trigger">
                             <div className="calculate-cube1">
@@ -102,7 +89,7 @@ const Faq = () => {
                                     className="ouiarrow-up-icon"
                                     alt=""
                                     src="/icons/arrowup.svg"
-                                    style={{transform: isVisible3 ? 'rotate(180deg)' : 'rotate(0deg)'}}
+                                    style={{transform: isVisible3 ? 'rotate(0deg)' : 'rotate(180deg)'}}
                                 />
                                 <div className="pattern-matcher">
                                     <div className="what-is-face">Can I use IdentityX for free?</div>
@@ -110,27 +97,26 @@ const Faq = () => {
                             </div>
                         </div>
                     </button>
-                    {isVisible3 && <p className="data-aggregator-p">
-                            <div className="data-aggregator">
-                                <div className="yes-identityx-offers">
-                                    Yes! IdentityX offers a 1-week free trial period. With a trial
-                                    subscription, users get the same level of security and
-                                    functionality as with a paid subscription.
-                                </div>
+                    {isVisible3 && <div id="text3" className="data-aggregator-p">
+                        <div className="data-aggregator">
+                            <div className="yes-identityx-offers">
+                                Yes! IdentityX offers a 1-week free trial period. With a trial
+                                subscription, users get the same level of security and
+                                functionality as with a paid subscription.
                             </div>
-                    </p>}
+                        </div>
+                    </div>}
                 </div>
                 <div className="pattern-parade">
                     <button className="button-faq" onClick={() => toggleVisibility({
-                        NumButton: 4,
-                        Setter: isVisible4
+                        Setter: setIsVisible4, Value: isVisible4
                     })}>
                         <div className="calculate-cube1">
                             <img
                                 className="ouiarrow-up-icon"
                                 alt=""
                                 src="/icons/arrowup.svg"
-                                style={{transform: isVisible4 ? 'rotate(180deg)' : 'rotate(0deg)'}}
+                                style={{transform: isVisible4 ? 'rotate(0deg)' : 'rotate(180deg)'}}
                             />
                             <div className="string-processor">
                                 <div className="what-is-face">
@@ -139,22 +125,21 @@ const Faq = () => {
                             </div>
                         </div>
                     </button>
-                    {isVisible4 && <p className="data-aggregator-p">
+                    {isVisible4 && <div id="text4" className="data-aggregator-p">
                         <div className="data-aggregator">
                             <div className="yes-identityx-has-container">
-                                Yes. IdentityX has a
+                                Yes. IdentityX has a&nbsp;
                                 <span className="day-money-back-guarantee">
                   14-day money-back guarantee
                 </span>
                                 &nbsp;no questions asked.
                             </div>
                         </div>
-                    </p>}
+                    </div>}
                 </div>
             </div>
         </div>
-    </section>
-;
+    </section>;
 };
 
 export default Faq;
