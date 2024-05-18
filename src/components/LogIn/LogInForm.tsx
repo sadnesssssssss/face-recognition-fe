32b7bf0f8c406149b7a5a22b70fd9e3a19406b3e
askdjfhasdkjfh <77674821+sadnesssssssss@users.noreply.github.com>
2024-05-19 01:42:25 +0300

    dockerfile and backend integration

--- src/components/LogIn/LogInForm.tsx
+++ src/components/LogIn/LogInForm.tsx
@@ -1,12 +1,19 @@
 import {FunctionComponent, memo, useCallback} from "react";
+import axios from 'axios'
 import {GoogleLogin} from '@react-oauth/google';
 import {jwtDecode} from 'jwt-decode'
 import "./LogInForm.css";
 
-const LogInForm: FunctionComponent = memo(() => {
-    const onButtonClick = useCallback(() => {
-        // Please sync "UserProfile" to the project
-    }, []);
+const LogInForm: FunctionComponent = () => {
+    const onButtonClick = () => {
+        const apiUrl = 'http://localhost:8000/api/user/get_base_resp/';
+        axios.get(apiUrl, {
+            responseType: "json"
+        }).then((resp: any) => {
+            const allPersons = resp.data;
+            console.log(allPersons);
+        });
+    };
 
 
     const responseMessage = (response: any) => {
@@ -18,27 +25,27 @@ const LogInForm: FunctionComponent = memo(() => {
     };
 
     return (<div className="form">
-            <div className="frame-div">
-                <div className="heading-1-sign-in-wrapper">
-                    <div className="heading-1">Sign In</div>
-                </div>
-                <input className="username" placeholder="Username" type="text"/>
-                <input className="password" placeholder="Password" type="text"/>
+        <div className="frame-div">
+            <div className="heading-1-sign-in-wrapper">
+                <div className="heading-1">Sign In</div>
             </div>
-            <div className="checkboxes2-deselected1-en-parent">
-                <label><input className="remember-checkbox" type="checkbox"></input>Remember me</label>
+            <input className="username" placeholder="Username" type="text"/>
+            <input className="password" placeholder="Password" type="text"/>
+        </div>
+        <div className="checkboxes2-deselected1-en-parent">
+            <label><input className="remember-checkbox" type="checkbox"></input>Remember me</label>
+        </div>
+        <div className="button-wrapper">
+            <button className="button" onClick={onButtonClick}>
+                <b className="log-in">Log In</b>
+            </button>
+            <p className="analogy">Or</p>
+            <div className="google-button">
+                <GoogleLogin onSuccess={responseMessage} onError={errorMessage}/>
             </div>
-            <div className="button-wrapper">
-                <button className="button" onClick={onButtonClick}>
-                    <b className="log-in">Log In</b>
-                </button>
-                <p className="analogy">Or</p>
-                <div className="google-button">
-                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-                </div>
-            </div>
+        </div>
 
-        </div>);
-});
+    </div>);
+};
 
 export default LogInForm;
