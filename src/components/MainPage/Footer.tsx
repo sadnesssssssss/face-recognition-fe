e2d4c5eb89df9dcaacd5d9ef15cb85ab6b04d0da
Елисеенко Владислав <ieliseenko79@gmail.com>
2024-05-29 20:27:18 +0300

    small fixes footer

--- src/components/MainPage/Footer.tsx
+++ src/components/MainPage/Footer.tsx
@@ -1,23 +1,31 @@
-
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer-child" />
-      <div className="reviews-parent">
-        <b className="reviews">Reviews</b>
-        <b className="about-team">About team</b>
-        <b className="check-key">Check key</b>
-        <div className="follow-us-parent">
-          <b className="follow-us">Follow us</b>
-          <div className="frame-container">
-            <img className="group-icon" alt="" src="/icons/contact-icons.svg" />
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
-};
+    return (
+            <footer className="footer">
+                <div className="footer-child"/>
+                <div className="reviews-parent">
+                    <b className="reviews">
+                        Reviews
+                    </b>
+                    <b className="about-team">
+                        About team
+                    </b>
+                    <b className="check-key">
+                        Check key
+                    </b>
+                    <div className="follow-us-parent">
+                        <b className="follow-us">Follow us</b>
+                        <div className="frame-container">
+                            <img className="icon" alt={""} src="/icons/vk.svg"/>
+                            <img className="icon" alt={""} src="/icons/telegram.svg"/>
+                            <img className="icon" alt={""} src="/icons/discord.svg"/>
+                            <img className="icon" alt={""} src="/icons/github.svg"/>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        );
+    };
 
-export default Footer;
+    export default Footer;
